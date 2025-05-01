3e601f6fc0ff89609b713e8a357b03a6deb5fdaa
amitihere <amiti.aneesh1@gmail.com>
2025-05-02 01:28:30 +0530

    fixed bugs

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,21 +11,21 @@ import instagram from '../../instagram-removebg-preview.png';
 const footer = () => {
   return (
     <div className='foot'>
-        <img className="footimg" src={perfectlogo} alt="Logo" style={{ width: '100%', height: 'auto', maxWidth: '200px' }} />
+        <img className="footimg" src={perfectlogo} alt="Logo" />
         <ul>
             <li><Link to="/" className="nav-link1" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>Home</Link></li>
             <li><Link to="/" className="nav-link1" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>Badminton</Link></li>
             <li><Link to="/tabletennis" className="nav-link1" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>TableTennis</Link></li>
         </ul>
         <div className='contact-info'>
-            <img src={locicon} alt="Location" style={{ width: '30px', height: '30px' }} />
+            <img src={locicon} alt="Location" />
             <h4>Opposite of Raddison Blu Resort, Rushikonda, Beach Rd, Yendada, Visakhapatnam, Andhra Pradesh 530045</h4>
         </div>
         <div className='contact'>
             <h3>Our Reach</h3>
             <br></br>
-            <img src={youtube} alt="YouTube" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
-            <img src={instagram} alt="Instagram" style={{ width: '40px', height: '40px' }} />
+            <img src={youtube} alt="YouTube" />
+            <img src={instagram} alt="Instagram" />
         </div>
         <h3 style={{color:"beige",position:"absolute",left:"56%",fontSize:"3vh",top:"60%",width:"30%",textAlign:"center"}}>Any other queries please contact us by +91 9878272846</h3>
         <h3 style={{color:"beige",position:"absolute",left:"57%",fontSize:"3vh",top:"75%",width:"30%",textAlign:"center"}}>dropShot.service@gmail.com</h3>
